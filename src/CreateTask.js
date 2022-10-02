f076c637bea50db31a37fb7ffc98148266f63f01
evolF35 <maccyair@gmail.com>
2022-10-01 21:42:44 -0700

    not sure y not working

--- src/CreateTask.js
+++ src/CreateTask.js
@@ -15,7 +15,6 @@ const taskFactory = (title,date,description,number,project) => {
 function createTask(title,date,description,number,project) {
 
     let task = taskFactory(title,date,description,number,project);
-    console.log(task);
     tasksArray.push(task);
 
 
